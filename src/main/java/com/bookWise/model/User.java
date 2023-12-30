a1428d591988df266179e04965d1c95d97c3ee98
Praveen <pravkumar.dev@gmail.com>
2023-12-30 19:41:41 +0530

    added autodeploy for sql commit

--- src/main/java/com/bookWise/model/User.java
+++ src/main/java/com/bookWise/model/User.java
@@ -1,48 +1,24 @@
 // Example User entity class
 package com.bookWise.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import java.util.Random;
+import lombok.Getter;
+import lombok.Setter;
 
+import javax.persistence.*;
+
+@Getter
+@Setter
 @Entity
 public class User {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "USER_ID")
+    private int userId;
 
-    private String username;
-    private String password;
+    @Column(name = "USER_NAME")
+    private String userName;
 
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public User(String username, String password) {
-        super();
-        this.username = username;
-        this.password = password;
-    }
+    @Column(name = "USER_PASSWORD")
+    private String userPassword;
 }
